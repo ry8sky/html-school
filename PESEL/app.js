96332168d3ef67c63a4fa0ce7baeff4feb503ed1
ry8sky <kalilgc@gmail.com>
2022-06-29 20:23:40 +0200

    Pesel check

--- PESEL/app.js
+++ PESEL/app.js
@@ -1,42 +1,63 @@
  var wpiszNrPESEL;
  var outputChkdPESEL;
 
-
-
  function kliknijSprawdz() {
     var button1 = document.querySelector("#button1");
     button1.addEventListener("click", wykonaj, false);
 }
-
  function wykonaj() {
     var wpiszNrPESEL = document.querySelector("#wpiszNrPESEL");
     var outputChkdPESEL = document.querySelector("#outputChkdPESEL")
-    outputChkdPESEL.innerHTML = wpiszNrPESEL.value;
+    var pesel = wpiszNrPESEL.value;
+    var rel = "";
+    rel += "Data urodzenia: " + zwrocDate(pesel) + "<br>";
+    rel += "Płeć: " + zwrocPlec(pesel) + "<br>";
+    rel += "Suma kontrola: " + sprawdzSumeKontrolna(pesel);
+    outputChkdPESEL.innerHTML = rel;
 }
+function zwrocDate(pesel) {
+    var y = parseInt(pesel.substring(0,2));
+    var m = parseInt(pesel.substring(2,4));
+    var d = parseInt(pesel.substring(4,6));
 
-pesel = "97081608113";
-
-console.log(pesel.substring(6,10));
-console.log(pesel.charAt(10));
-
-var y = pesel.substring(0,2);var m = pesel.substring(2,4);var d = pesel.substring(4,6);
-console.log(parseInt(y)+"-"+parseInt(m)+"-"+parseInt(d));
-
+    var dekada;
+    switch(Math.floor(m/20)) {
+        case 0:
+            dekada = 1900;
+            break;
+        case 1:
+            dekada = 2000;
+            break;
+        case 2:
+            dekada = 2100;
+            break;
+        case 3:
+            dekada = 2200;
+            break;
+        case 4:
+            dekada = 1800;
+            break;
+        default:
+            dekada = 0;
+            break;
+    }
+    m%= 20;
+    return (dekada+y).toString() + "-" + m.toString() + "-" + d.toString(); 
+}
+function zwrocPlec(pesel) {
 var sexMW = pesel.charAt(9);
-function sex() {
     if (sexMW % 2 == 0) {
-    return "W"; 
-   }    else 
-    return "M";
+        return "W"; 
+   }  else  return "M";
 }
-console.log(sex);
-
-function suma() {
-var suma = pesel.charAt(0)*1+pesel.charAt(1)*3+pesel.charAt(2)*7+pesel.charAt(3)*9+pesel.charAt(4)*1+pesel.charAt(5)*3+pesel.charAt(6)*7+pesel.charAt(7)*9+pesel.charAt(8)*1+pesel.charAt(9)*3+pesel.charAt(10)*1;
-    if (suma.charAt(2) == 0) {
-        return "Suma kontrolna: Ok!"
-    } else 
-    return "Suma kontrolna: Błąd!"
+function sprawdzSumeKontrolna(pesel) {
+    let wagi = [1,3,7,9,1,3,7,9,1,3,1]
+    var suma = 0;
+    for (let i = 0; i < wagi.length; i++) {
+        suma += (parseInt(pesel[i]) * wagi[i])
+    }
+    if (suma % 10 == 0) return "Ok!";
+    else return "Błąd!";
 }
 
 window.addEventListener("load", kliknijSprawdz, false);
